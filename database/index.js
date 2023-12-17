e43bcab33c459a9a2008b420732937b416c2e0ef
Fauzan Azim <92430909+Fauzan-azim@users.noreply.github.com>
2023-12-17 19:33:42 +0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,18 +1,34 @@
-const mysql = require('mysql2')
+const mysql = require("mysql");
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-pool.getConnection((err, conn) => {
-    if(err) console.log(err)
-    console.log("Connected successfully")
-})
+con.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
 
-module.exports = pool.promise()
+function query(sql, values) {
+  return new Promise((resolve, reject) => {
+    con.query(sql, values, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
+
+module.exports = query;
