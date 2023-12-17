223b33abee64087e933e854afa0f5f46b5b03531
Fauzan Azim <92430909+Fauzan-azim@users.noreply.github.com>
2023-12-17 19:34:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const authRouter = require("./routes/auth.router");
 
 app.use("/auth", authRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,() => {
   console.log(`Server is running on  port ${PORT}`);
