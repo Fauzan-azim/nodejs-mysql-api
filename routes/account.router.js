0c215f927ba6451f5f12dfc18b89593153dd2aa6
Fauzan Azim <92430909+Fauzan-azim@users.noreply.github.com>
2023-12-17 21:43:20 +0700

    Update account.router.js

--- routes/account.router.js
+++ routes/account.router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const accountController = require("../controller/account.controller");
 
 router.post("/updateNIK", accountController.updateNIK);
+router.post("/updateAccount", accountController.updateAccount);
 
 module.exports = router;
