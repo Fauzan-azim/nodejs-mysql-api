e94b508c40ce96a438e74ec2e55ecef5472aa941
Fauzan Azim <92430909+Fauzan-azim@users.noreply.github.com>
2023-12-17 21:43:49 +0700

    add updateAccount endpoint

--- controller/account.controller.js
+++ controller/account.controller.js
@@ -17,6 +17,27 @@ const accountController = {
       return res.status(500).json({ error: "Internal Server Error" });
     }
   },
+
+  updateAccount: async (req, res) => {
+    const { email, fullname, new_password, id } = req.body;
+    if (!email || !fullname || !new_password) {
+      return res
+        .status(400)
+        .json({ error: "Email, name, and new_password are required" });
+    }
+    try {
+      const hashedPassword = await bcrypt.hash(new_password, 10);
+
+      const updateQuery =
+        "UPDATE users SET email = ?, fullname = ?, password = ? WHERE id = ?";
+      await query(updateQuery, [email, fullname, hashedPassword, id]);
+
+      return res.json({ success: true, message: "Update successful" });
+    } catch (error) {
+      console.error("Error during update:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
 };
 
 module.exports = accountController;
