066b9f7c0aa724ecc9eafd8379a60fca4c5e468b
Fauzan Azim <92430909+Fauzan-azim@users.noreply.github.com>
2023-12-17 19:34:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-const postsRouter = require('./routes/posts.router')
-const authRouter = require('./routes/auth.router')
+const authRouter = require("./routes/auth.router");
 
-app.use("/api/v1/posts", postsRouter)
-app.use("/api/v1/auth", authRouter)
+app.use("/auth", authRouter);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Server is running....")
-})
+app.listen(PORT,() => {
+  console.log(`Server is running on  port ${PORT}`);
+});
